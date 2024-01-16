c2647fde6ae9e232e1becb9f29ad9d47ba62d36e
usedhondacivic <micacer48@gmail.com>
2024-01-15 21:48:15 -0700

    Gerbers for v1.1 order

--- HARDWARE/motor_board.py
+++ HARDWARE/motor_board.py
@@ -1,10 +1,33 @@
 # Author: Michael Crum
-# Date: 11/14/23
-# Circuit description for Tiny POV's motor and power supply board.
-# Regulates motor speed + uses a self regulating flyback converter to transfer power to the arm.
-# Self reference: https://electronics.stackexchange.com/questions/506874/simple-self-oscillating-flyback-converter
-
+# Date: 11/12/24
+# Inductive power supply + motor speed controller.
+# Uses a full bridge resonance converter:
+# https://www.we-online.com/components/media/o122593v410%20ANP070_EN_Proprietary_wireless_power_transfer_solution_for_high_performance_including_data_transmission.pdf
 
 from skidl import *
 
 set_default_tool(KICAD7)
+
+# TEMPLATES
+
+C_TH = Part(
+    "tiny_pov_symbols",
+    "C_Polarized",
+    dest=TEMPLATE,
+    footprint="tiny_pov_footprints:C_polarized_TH",
+)
+
+# INTERFACES
+
+# SUBCIRCUITS
+
+# 555 Timer
+
+# Full Bridge Driver
+# https://fscdn.rohm.com/en/products/databook/datasheet/ic/motor/dc/bd622x-e.pdf
+
+# Motor driver
+
+# EXPORTING
+ERC()
+generate_netlist()
